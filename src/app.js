0235d9a79ead33e32042954512d38498be7b06d9
U Prashanth <you@example.com>
2018-07-15 17:55:25 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
+
+ReacDOM.render(<p>Hello World!!!</p>, document.getElementById('app'));
