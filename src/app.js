2176742f507cae037f6e473dd7c6c5b58311625b
U Prashanth <you@example.com>
2018-07-15 19:06:13 +0530

    Configured React-Router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+// Modules
 import React from 'react';
 import ReacDOM from 'react-dom';
 
-import 'normalize.css/normalize.css'
+// Styles
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReacDOM.render(<p>Hello World!!!</p>, document.getElementById('app'));
+// App Components
+import AppRouter from './routes/AppRouter';
+
+ReacDOM.render(<AppRouter/>, document.getElementById('app'));
